75712a48c170ef280dcddb3dcf76d536881d5340
remtrik <remtrik@mail.ru>
2025-08-05 17:32:16 +0300

    finally change sdp.kt

--- app/src/main/java/com/remtrik/m3khelper/util/sdp.kt
+++ app/src/main/java/com/remtrik/m3khelper/util/sdp.kt
@@ -1,86 +1,49 @@
 package com.remtrik.m3khelper.util
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.remtrik.m3khelper.util.SdpScreenDimensionValues.smallestWidth
-
-internal object SdpScreenDimensionValues {
-    var smallestWidth: Int = 0
-}
 
 // Assigns values to the variables above
 @Composable
-private fun AssValsToTheSdpObject() {
+private fun rememberApproximatedWidth(): Int {
     val config = LocalConfiguration.current
-    smallestWidth = config.smallestScreenWidthDp
+    val smallestWidth = config.smallestScreenWidthDp
+    return remember(smallestWidth) { approximateWidth(smallestWidth) }
 }
 
 // Will return the smallestWidth approximated to nearest 30 to improve performance
 private fun approximateWidth(value: Int): Int {
-    val remainder = value % 30
-    return when {
-        remainder <= 15 -> {
-            value - remainder
-        }
-
-        else -> {
-            value + (30 - remainder)
-        }
-    }
+    return (value + 15) / 30 * 30
 }
 
 @Composable
 fun Int.sdp(): Dp {
-    if (smallestWidth == 0) {
-        AssValsToTheSdpObject()
-    }
-    val ratio = when {
-        smallestWidth <= 400 -> {
-            approximateWidth(smallestWidth) / 440.0
-        }
-
-        smallestWidth <= 450 -> {
-            approximateWidth(smallestWidth) / 450.0
-        }
-
-        smallestWidth <= 550 -> {
-            approximateWidth(smallestWidth) / 450.0
-        }
-
-        else -> {
-            approximateWidth(smallestWidth) / 650.0
+    val approxWidth = rememberApproximatedWidth()
+    val ratio = remember(approxWidth) {
+        when {
+            approxWidth <= 400 -> approxWidth / 440.0
+            approxWidth <= 550 -> approxWidth / 450.0
+            else -> approxWidth / 650.0
         }
     }
-    val final = this * ratio
-    return final.dp
+    return (this * ratio).dp
 }
 
 @Composable
 fun Int.ssp(): TextUnit {
-    if (smallestWidth == 0) {
-        AssValsToTheSdpObject()
-    }
-    val ratio = when {
-        smallestWidth <= 400 -> {
-            approximateWidth(smallestWidth) / 500.0
-        }
-
-        smallestWidth <= 450 -> {
-            approximateWidth(smallestWidth) / 450.0
-        }
-
-        smallestWidth <= 550 -> {
-            approximateWidth(smallestWidth) / 500.0
-        }
-
-        else -> {
-            approximateWidth(smallestWidth) / 650.0
+    val approxWidth = rememberApproximatedWidth()
+    val ratio = remember(approxWidth) {
+        when {
+            approxWidth <= 400 -> approxWidth / 500.0
+            approxWidth <= 450 -> approxWidth / 450.0
+            approxWidth <= 550 -> approxWidth / 500.0
+            else -> approxWidth / 650.0
         }
     }
-    val final = this * ratio
-    return final.sp
+    return (this * ratio).sp
 }
